c05c69e482a1b3e86d0164099c25c0cfa2d2a0ca
Rahul Sondhiya <your-email@example.com>
2025-12-15 22:58:10 +0530

    04-cards-project

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -1,40 +1,128 @@
-
-import React from 'react'
-import {Bookmark} from 'lucide-react'
+import Card from './components/Card'
 
 const App = () => {
+ 
+  const jobOpenings = [
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/1200px-Google_%22G%22_logo.svg.png",
+    companyName: "Google",
+    datePosted: "5 days ago",
+    post: "Frontend Software Engineer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$45/hr",
+    location: "Bengaluru, India"
+  },
+  {
+    brandLogo: "https://substackcdn.com/image/fetch/$s_!G1lk!,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F8ed3d547-94ff-48e1-9f20-8c14a7030a02_2000x2000.jpeg",
+    companyName: "Apple",
+    datePosted: "2 weeks ago",
+    post: "iOS Developer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$60/hr",
+    location: "Hyderabad, India"
+  },
+  {
+    brandLogo: "https://pngimg.com/d/meta_PNG12.png",
+    companyName: "Meta",
+    datePosted: "1 week ago",
+    post: "React Engineer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$55/hr",
+    location: "Remote"
+  },
+  {
+    brandLogo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkQZwfIF1UdjlMBtjg4fmlfdGU0suXMgG5qA&s",
+    companyName: "Amazon",
+    datePosted: "10 days ago",
+    post: "Backend Developer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$40/hr",
+    location: "Chennai, India"
+  },
+  {
+    brandLogo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKBhwWT_lkLxqE389PVpukWurM-fT4vMdqBA&s",
+    companyName: "Netflix",
+    datePosted: "3 weeks ago",
+    post: "Platform Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$70/hr",
+    location: "Remote"
+  },
+  {
+    brandLogo: "https://static.vecteezy.com/system/resources/previews/027/127/473/non_2x/microsoft-logo-microsoft-icon-transparent-free-png.png",
+    companyName: "Microsoft",
+    datePosted: "4 days ago",
+    post: "Cloud Engineer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$50/hr",
+    location: "Noida, India"
+  },
+  {
+    brandLogo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTskpgn2b2OSzf6IP6kF4DroEX4ivtkUmNQhg&s",
+    companyName: "Tesla",
+    datePosted: "2 months ago",
+    post: "Software Engineer – Autopilot",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$65/hr",
+    location: "Pune, India"
+  },
+  {
+    brandLogo: "https://blog.logomaster.ai/hs-fs/hubfs/ibm-logo-1956.jpg?width=672&height=454&name=ibm-logo-1956.jpg",
+    companyName: "IBM",
+    datePosted: "6 days ago",
+    post: "AI Engineer",
+    tag1: "Part Time",
+    tag2: "Junior Level",
+    pay: "$35/hr",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnOVjTWaheo4E99cgYZ6y14tpsgHlm0VN8Hw&s",
+    companyName: "Oracle",
+    datePosted: "8 weeks ago",
+    post: "Database Engineer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$48/hr",
+    location: "Bengaluru, India"
+  },
+  {
+    brandLogo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRccHiUD903piWYkwZyVe2NHHdYUufyzPOybQ&s",
+    companyName: "Adobe",
+    datePosted: "9 days ago",
+    post: "UI/UX Engineer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$42/hr",
+    location: "Noida, India"
+  }
+];
+ 
+ console.log(jobOpenings);
+
   return (
-    <div className='parent'>
-      <div className='card'>
-        <div>
-          <div className="top">
-          <img src='https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg' alt='' />
-          <button>Save <Bookmark size={10} /> </button>
+    <div className="parent">
+      {jobOpenings.map(function(elem, idx) { 
+        return  <div key={idx}>
+          <Card 
+          company={elem.companyName} 
+          datePosted={elem.datePosted}
+          post={elem.post}
+          tag1={elem.tag1}
+          brandLogo={elem.brandLogo}
+          pay={elem.pay}
+          tag2={elem.tag2} />
         </div>
-        <div className="center">
-          <h3>Amazon <span>5 Days ago</span></h3>
-          <h2>Senior UI/UX Designer</h2>
-          <div className='tag'>
-            <h4>Part Time</h4>
-            <h4>Senior Level</h4>
-          </div>
-        </div>
-        </div>
-        <div className="bottom">
-          <div>
-            <h3>$120/hr</h3>
-            <p>Mumbai, India</p>
-          </div>
-            <button>Apply Now</button>
-        </div>
-      </div>
-
-      
-
+      })}
     </div>
   )
 }
 
 export default App
-
-// https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg
