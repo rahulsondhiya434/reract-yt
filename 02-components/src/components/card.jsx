3b3b018df5944585412690026efe596a9efa69b2
rahulsondhiya434 <rbai6269@gmail.com>
2025-12-21 10:15:47 +0530

    Delete 02-components directory

--- 02-components/src/components/card.jsx
+++
@@ -1,8 +0,0 @@
-function Card() {
-    return <div className='card'>
-         <h3>Rahul Sondhiya</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, reiciendis.</p>
-      </div>
-}
-
-export default Card;
