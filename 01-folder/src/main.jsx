8ee5b7bb727c8ea8902e9a8ea870d4de6da18ee8
rahulsondhiya434 <rbai6269@gmail.com>
2025-12-21 10:13:56 +0530

    Delete 01-folder directory

--- 01-folder/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import './App.css'
-import App from './App.jsx'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
-<App />
-)
